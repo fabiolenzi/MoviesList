59336b4ea8cd748404f42bb3b91320a5f7b5744b
Fábio Lenzi <fabio@lenzi.me>
2020-08-18 15:51:37 -0300

    Initial Commit

---
+++ MoviesList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesList
+//
+//  Created by Fábio Lenzi on 18/08/20.
+//  Copyright © 2020 Fábio Lenzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
