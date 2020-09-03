6391104b910ecf9ff350fb93f1a27994258150b9
Fábio Lenzi <fabio@lenzi.me>
2020-09-03 18:59:36 -0300

    Fix Details view paddings and make title and overview multilines.

--- MoviesList/View/MovieDetailsView.swift
+++ MoviesList/View/MovieDetailsView.swift
@@ -12,8 +12,8 @@ class MovieDetailsView: UIViewController {
     
     private var posterImage = UIImageView()
     private var titleLabel = UILabel()
-    private var overviewLabel = UILabel()
     private var scoreLabel = UILabel()
+    private var overviewLabel = UILabel()
     private var movie: Movie
     
     init(movie: Movie) {
@@ -43,9 +43,12 @@ class MovieDetailsView: UIViewController {
                 print(error)
             }
         })
+        
         titleLabel.text = movie.title
-        overviewLabel.text = movie.overview
+        titleLabel.numberOfLines = 0
         scoreLabel.text = "Score: \(movie.score)" //TODO: Localize it
+        overviewLabel.text = movie.overview
+        overviewLabel.numberOfLines = 0
         
         view.addSubview(posterImage)
         view.addSubview(titleLabel)
@@ -65,25 +68,25 @@ class MovieDetailsView: UIViewController {
             
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: MovieDetailsView.viewPadding),
             titleLabel.leadingAnchor.constraint(equalTo: posterImage.trailingAnchor, constant: MovieDetailsView.viewPadding),
-            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: MovieDetailsView.viewPadding),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -MovieDetailsView.viewPadding),
             titleLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: MovieDetailsView.titleHeight),
             
             scoreLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: MovieDetailsView.viewPadding),
             scoreLabel.leadingAnchor.constraint(equalTo: posterImage.trailingAnchor, constant: MovieDetailsView.viewPadding),
-            scoreLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: MovieDetailsView.viewPadding),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -MovieDetailsView.viewPadding),
             scoreLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: MovieDetailsView.titleHeight),
             
             overviewLabel.topAnchor.constraint(equalTo: posterImage.bottomAnchor, constant: MovieDetailsView.viewPadding),
             overviewLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: MovieDetailsView.viewPadding),
-            overviewLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: MovieDetailsView.viewPadding),
-            overviewLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: MovieDetailsView.viewPadding)
+            overviewLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -MovieDetailsView.viewPadding),
+            overviewLabel.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -MovieDetailsView.viewPadding)
         ])
     }
     
     // MARK: - UI Constants
     
     private static let titleHeight: CGFloat = 40
-    private static let posterHeight: CGFloat = 80
-    private static let posterWidth: CGFloat = 60
+    private static let posterHeight: CGFloat = 160
+    private static let posterWidth: CGFloat = 120
     private static let viewPadding: CGFloat = 10
 }
